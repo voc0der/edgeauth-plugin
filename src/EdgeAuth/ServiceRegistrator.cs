c5e4fe152b7e8b74815ae15888a890fc3d92c567
voc0der <github@netspace.in>
2025-09-26 18:32:56 +0000

    Update ServiceRegistrator.cs

--- src/EdgeAuth/ServiceRegistrator.cs
+++ src/EdgeAuth/ServiceRegistrator.cs
@@ -1,4 +1,4 @@
-using MediaBrowser.Common.Plugins;       // IPluginServiceRegistrator
+using Jellyfin.Common.Plugins;              // <-- correct namespace for IPluginServiceRegistrator
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,16 +8,13 @@ namespace Jellyfin.Plugin.EdgeAuth
     {
         public void RegisterServices(IServiceCollection services)
         {
-            // Core services for EdgeAuth
+            // Core services
             services.AddSingleton<IEdgeAuthStore, MemoryEdgeAuthStore>();
             services.AddSingleton<EdgeAuthServer>();
 
             // Expose the current plugin configuration instance from Jellyfin
-            // Note: Jellyfin will construct Plugin and set Configuration; this adds that instance to DI.
             services.AddSingleton(sp =>
             {
-                // Resolve the Plugin itself from Jellyfin’s plugin system
-                // (Jellyfin adds BasePlugin<T> to DI). If not available, we still want the app to boot.
                 var plugin = sp.GetService(typeof(Plugin)) as Plugin;
                 return (PluginConfiguration)(plugin?.Configuration ?? new PluginConfiguration());
             });
